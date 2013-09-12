473f47f14e1dee2dc3a90fbfc45370d3db60328c
Jamuna Gopi <ggjamswe144@gmail.com>
2013-09-12 22:49:50 +0530

    Delete CalcTax.h

---  TaxCalculator(LAB2)/CalcTax.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h >
-#import "TaxProto.h"
-#import "Bill.h"
-@interface CalcTax:NSObject <TaxProto>//Adopts the protocol.
-
--(void)calc:(id) b;//Dynamically accessing the object.
-
-@end
